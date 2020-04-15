4639f5012da72b4d4eb148982a7548fc0953a50d
KatieMCook <bskmc@leeds.ac.uk>
2020-04-15 14:49:40 +0100

    making dif sites for PCA df

--- mollusc_sdm.R
+++ mollusc_sdm.R
@@ -1544,5 +1544,33 @@ p1
 p1+layer(sp.polygons(japan_outline))
 
 
+### GET DF FOR PCA ----
+#extract dif at lat lons
+lonlat<-data.frame(lon=latlon$lon, lat=latlon$lat)
+
+for (i in 1:length(dif_list85)){
+  dif_sites<- extract(dif_list85[[i]], lonlat )
+  dif_sites<-data.frame(site= latlon$Site, change= dif_sites)
+  assign(paste0('dif_sites', i), dif_sites)
+}
+
+rm(dif_sites)
+dif_site_list<- lapply(ls(pattern='dif_sites'), get)
+
+#now add on group number to list and unlist into one big df.
+for(i in 1:length(dif_site_list)){
+  dif_site_list[[i]]$group<-paste0('mollusc', i)
+}
+
+dif_site_all<-do.call(rbind, dif_site_list)
+
+#rm groups with are NaN
+torm<- which(dif_site_all$change=='NaN')
+
+dif_site_all<- dif_site_all[-c(torm), ]
+
+#now write csv
+write.csv(dif_site_all, 'dif_site_mollusc.csv')
+
 
 
